c1e3bbd6946a8778914034b3657043a09357e13b
Praful Nigam <prafulnigam16@gmail.com>
2024-05-30 13:21:26 +0530

    Minor Fixes

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,7 @@ const app = express();
 const port = 8080;
 
 
-app.use(cors(
-    {
-      origin: ["https://audio-nest-api.vercel.app"],
-      methods: ["POST", "GET", "DELETE", "PUT"],
-      credentials: true
-    }
-  ));
+app.use(cors());
 app.use(express.json());
 
 // connect mongodb to our node app.
